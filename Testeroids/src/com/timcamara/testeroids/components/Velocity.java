d263674e689d1747276f55dfa03af77946880a35
Tim <tcamara21@gmail.com>
2013-08-12 20:45:17 -0700

    Checkpoint in case shit goes down

--- Testeroids/src/com/timcamara/testeroids/components/Velocity.java
+++ Testeroids/src/com/timcamara/testeroids/components/Velocity.java
@@ -2,52 +2,48 @@ package com.timcamara.testeroids.components;
 
 import com.artemis.Component;
 import com.artemis.utils.TrigLUT;
+import com.artemis.utils.FastMath;
+import com.badlogic.gdx.math.Vector2;
 
 public class Velocity extends Component {
-	public float x;
-	public float y;
-	public float x_max;
-	public float y_max;
+	public Vector2 speed;
+	public float   speed_max;
+	public float   accel;
+	public float   friction;
 	
-	public Velocity(float x_max, float y_max) {
-		this(0, 0, x_max, y_max);
+	public Velocity(float speed, float angle, float speed_max) {
+		this(speed, angle, speed_max, 0, 0);
 	}
 	
-	public Velocity(float x, float y, float x_max, float y_max) {
-		this.x = x;
-		this.y = y;
-		this.x_max = x_max;
-		this.y_max = y_max;
+	public Velocity(float speed, float angle, float speed_max, float accel, float friction) {
+		float x = speed * TrigLUT.cosDeg(angle + 90);
+		float y = speed * TrigLUT.sinDeg(angle + 90);
+		
+		this.speed = new Vector2(x, y);
+		this.speed_max = speed_max;
+		this.accel = accel;
+		this.friction = friction;
 	}
 	
 	public void add(float speed, float angle) {
-		x += speed * TrigLUT.cosDeg(angle + 90);
-		y += speed * TrigLUT.sinDeg(angle + 90);
+		this.speed.x += speed * TrigLUT.cosDeg(angle + 90);
+		this.speed.y += speed * TrigLUT.sinDeg(angle + 90);
 		
 		// Make sure we don't exceed limit
 		limiter();
 	}
 	
 	public void set(float speed, float angle) {
-		x = speed * TrigLUT.cosDeg(angle + 90);
-		y = speed * TrigLUT.sinDeg(angle + 90);
+		this.speed.x = speed * TrigLUT.cosDeg(angle + 90);
+		this.speed.y = speed * TrigLUT.sinDeg(angle + 90);
 		
 		// Make sure we don't exceed limit
 		limiter();
 	}
 	
 	private void limiter() {
-		if(x > x_max) {
-			x = x_max;
-		}
-		if(y > y_max) {
-			y = y_max;
-		}
-		if(x < -x_max) {
-			x = -x_max;
-		}
-		if(y < -y_max) {
-			y = -y_max;
+		if(speed.len() > speed_max) {
+			speed.mul(speed_max / speed.len());
 		}
 	}
 }
